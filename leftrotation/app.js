4230f8f935220dda06946fff35b9074260de04a5
Ing. Raúl Adolfo Torres Vargas <ingrauladolfo.torresvargas@gmail.com>
2021-11-23 12:26:35 -0600

    finish left rotation

--- leftrotation/app.js
+++ leftrotation/app.js
@@ -1,16 +1,39 @@
-const rotationLeft = (arr, rotations) => {
-  const rotatedArray = arr.concat();
-  for (let i = 0; i < rotations.length; i++) {
-    const frontItem = rotatedArray.shift();
-    rotatedArray.push(frontItem);
+const leftRotate = (arr, d, n) => {
+  for (let i = 0; i < d; i++) {
+    leftRotateByOne(arr, n);
   }
-  return rotatedArray;
+};
+const leftRotateByOne = (arr, n) => {
+  let i,
+    temp = arr[0];
+  for (i = 0; i < n - 1; i++) {
+    arr[i] = arr[i + 1];
+  }
+  arr[i] = temp;
+};
+const printArray = (arr, n) => {
+  for (let i = 0; i < n; i++) {
+    console.log(arr[i] + " ");
+  }
 };
 
-const numRotation = 4;
-const sampleArray = [1, 2, 3, 4, 5];
+let sampleArray = [1, 2, 3, 4, 5];
+let numberOfRotations = 4;
+console.log(leftRotate(sampleArray, numberOfRotations, sampleArray.length));
+console.log(printArray(sampleArray, sampleArray.length));
+// const rotationLeft = (arr, rotations) => {
+//   const rotatedArray = arr.concat();
+//   for (let i = 0; i < rotations.length; i++) {
+//     const frontItem = rotatedArray.shift();
+//     rotatedArray.push(frontItem);
+//   }
+//   return rotatedArray;
+// };
 
-const expectedOutput = [5, 1, 2, 3, 4];
+// const numRotation = 1;
+// const sampleArray = [1, 2, 3, 4, 5];
 
-console.log(`Array with roation: ${rotationLeft(sampleArray, numRotation)}`);
-console.log(`Expected output: ${expectedOutput}`);
+// // const expectedOutput = [5, 1, 2, 3, 4];
+
+// console.log(`Array with roation:`);
+// console.log(`Expected output: ${expectedOutput}`);
